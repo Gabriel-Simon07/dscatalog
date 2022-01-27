1608cf64331dae5eadde9876125330ac72863d90
Gabriel Simon <gabrielsimon775@gmail.com>
2022-01-27 20:38:57 -0300

    Adicionados registros para o banco h2 (Seeding database)

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO table_category (name) VALUES ('Computador');
+INSERT INTO table_category (name) VALUES ('Máquina de lavar');
+INSERT INTO table_category (name) VALUES ('Smartphone');
+INSERT INTO table_category (name) VALUES ('Teclado Gamer');
+INSERT INTO table_category (name) VALUES ('Notebook');
