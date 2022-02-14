d7e76463278b3b4dec06eda38db9c4729aedf099
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-14 20:48:39 -0300

    Implemented DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+public class CategoryDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	
+	public CategoryDTO() {
+		
+	}
+
+	public CategoryDTO(Long id, String nome) {
+		this.id = id;
+		this.name = nome;
+	}
+	
+	public CategoryDTO(Category entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return name;
+	}
+
+	public void setNome(String nome) {
+		this.name = nome;
+	}
+
+	
+}
